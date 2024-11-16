d777d0c3dc2e65afa85760fd12e085fce5ede579
Md-Sojib-Hossain-cse <sojibhossain.cse@gmail.com>
2024-11-16 23:49:29 +0600

    express router

--- project-1/dist/app/app.js
+++ project-1/dist/app/app.js
@@ -8,6 +8,29 @@ const app = (0, express_1.default)();
 //parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+//Routers 
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/course", courseRouter);
+userRouter.post("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "User created successfully",
+        data: user,
+    });
+});
+courseRouter.post("/create-course", (req, res) => {
+    const courseData = req.body;
+    console.log(courseData);
+    res.json({
+        success: true,
+        message: "User created successfully",
+        data: courseData,
+    });
+});
 //Middleware
 const logger = (req, res, next) => {
     console.log(req.url, req.method, req.cookies, req.body, req.hostname);
--- project-1/src/app/app.ts
+++ project-1/src/app/app.ts
@@ -5,6 +5,34 @@ const app = express()
 app.use(express.json())
 app.use(express.text())
 
+//Routers 
+const userRouter = express.Router();
+const courseRouter = express.Router();
+
+app.use("/api/v1/users" , userRouter)
+app.use("/api/v1/course" , courseRouter)
+
+userRouter.post("/create-user" , (req : Request , res : Response) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success : true,
+        message : "User created successfully",
+        data : user,
+    })
+})
+
+
+courseRouter.post("/create-course" , (req : Request , res : Response)=> {
+    const courseData = req.body;
+    console.log(courseData)
+    res.json({
+        success : true,
+        message : "User created successfully",
+        data : courseData,
+    })
+})
+
 //Middleware
 const logger = (req : Request , res : Response , next : NextFunction) => {
     console.log(req.url , req.method , req.cookies , req.body , req.hostname)
